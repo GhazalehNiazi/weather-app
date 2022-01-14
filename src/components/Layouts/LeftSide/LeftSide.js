a675b6c079547e9ee3ddbc719ae93999f0458ec3
GhazalehNiazi <ghazaleniazisabet02@gmail.com>
2022-01-14 19:39:10 +0330

    :added a weather API

--- src/components/Layouts/LeftSide/LeftSide.js
+++ src/components/Layouts/LeftSide/LeftSide.js
@@ -1,6 +1,7 @@
 import SearchBox from "./SearchBox";
 import WeatherReport from "./WeatherReport";
 import classes from "./LeftSide.module.css";
+import Api from "../../Api";
 
 const LeftSide = () => {
   return (
@@ -9,6 +10,7 @@ const LeftSide = () => {
       <h1 className={classes.title}>
         Weather <span>Forecast</span>
       </h1>
+      <Api/>
       <div className={classes.imgContainer}>
         <div>img</div>
         <div>img</div>
