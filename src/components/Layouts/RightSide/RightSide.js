df5920d23e0d4b3f00f209c17a062901d7f660cb
GhazalehNiazi <ghazaleniazisabet02@gmail.com>
2022-01-22 14:21:35 +0330

    :made the data dynamic

--- src/components/Layouts/RightSide/RightSide.js
+++ src/components/Layouts/RightSide/RightSide.js
@@ -1,52 +1,10 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext,  useState } from "react";
 import weatherContext from "../../../store/weather-context";
 import classes from "./RightSide.module.css";
-import { ApiCaller } from "../../../Api";
 
 const RightSide = () => {
-  const context = useContext(weatherContext);
-  //  context.searchForCity();
-  // useEffect(()=>context.searchForCity(),context);
-  // setTimeout(()=>console.log(context),5000)
-  //   const [call , setCall] = useState(true)
-  //   if(call){
-  //   ApiCaller()
-  //    setCall(false);
-  // }
-  //   const [call , setCall] = useState(true)
-  //   if(call){
-  //     context.searchForCity();
-  //    setCall(false);
-  // }
-  // let componentMounted = true;
-  // let data;
-  // console.log("its changin the location");
-  // const url =
-  //   "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=3960092f469b92d98917ccf6db87f3ba";
-  // useEffect(() => {
-  //   console.log("hey1");
-  //   const fetchWeather = async () => {
-  //     console.log("here");
-  //     const response = await fetch(url);
-  //     if (componentMounted) {
-  //       data = await response.json();
-  //     }
-  //     return () => {
-  //       componentMounted = false;
-  //     };
-  //   };
-  //   fetchWeather();
-  // }, []);
-
-
-
-  // console.log(data);
-
-  // if (data) {
-  //   console.log("it has data");
-  // } else {
-  //   console.log(" no data");
-  // }
+  const {timezone, temp, feelsLike, sunset, sunrise} = useContext(weatherContext);
+  
 
   return (
     <div className={classes.container}>
@@ -59,12 +17,12 @@ const RightSide = () => {
           <h5>sat ,1Aug</h5>
         </div>
         <div className={classes.main}>
-          <h1>28</h1>
-          <h5>Tehran, Iran</h5>
+          <h1>{temp}</h1>
+          <h5>{timezone}</h5>
         </div>
         <div className={classes.underMain}>
-          <h5>feels like 32</h5>
-          <h5>sunset 20:15</h5>
+          <h5>feels like {feelsLike}</h5>
+          <h5>sunset {sunrise}</h5>
         </div>
       </div>
       <div className={classes.rainChance}>
