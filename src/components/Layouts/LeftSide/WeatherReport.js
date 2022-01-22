0a1ed5bea47ccd9092ad65d15150c2584814a70b
GhazalehNiazi <ghazaleniazisabet02@gmail.com>
2022-01-22 19:59:54 +0330

    :added daily weather api detail

--- src/components/Layouts/LeftSide/WeatherReport.js
+++ src/components/Layouts/LeftSide/WeatherReport.js
@@ -1,24 +1,34 @@
-import classes from './WeatherReport.module.css';
-
-const WeatherReport=()=>{
-return(
+import { useContext } from "react";
+import weatherContext from "../../../store/weather-context";
+import classes from "./WeatherReport.module.css";
+import { weekDays } from "../../helper/year";
+const WeatherReport = () => {
+  const { daily } = useContext(weatherContext);
+  console.log(daily);
+  const weekday = weekDays.map((day, index) => {
+    console.log(day);
+    // TODO fix the week days index based on today
+    return (
+      <div key={day} className={classes.day}>
+        {day}
+        <div>{daily && `${daily[index].weather[0].description}`}</div>
+        <div>{daily && `${daily[index].clouds}`}</div>
+        <div>{daily && `${daily[index].temp.day}`}</div>
+        <div>{daily && `${daily[index].feels_like.day}`}</div>
+      </div>
+    );
+  });
+  console.log(weekday);
+  return (
     <div>
-        <div className={classes.timeDeclation}>
-            <div>Week</div>
-            <div>Month</div>
-            <div>3 month</div>
-            <div>6 month</div>
-        </div>
-        <div className={classes.weekDays} >
-            <div >Saturday</div>
-            <div>Sunday</div>
-            <div>Monday</div>
-            <div>Tuesday</div>
-            <div>Wednesday</div>
-            <div>Thursday</div>
-            <div>Friday</div>
-        </div>
+      <div className={classes.timeDeclation}>
+        <div>Week</div>
+        <div>Month</div>
+        <div>3 month</div>
+        <div>6 month</div>
+      </div>
+      <div className={classes.weekDays}>{weekday}</div>
     </div>
-)
-}
+  );
+};
 export default WeatherReport;
--- src/components/Pages/MainPage.js
+++ src/components/Pages/MainPage.js
@@ -11,27 +11,30 @@ const MainPage = () => {
   const [feelsLike, setFeelsLike] = useState(null);
   const [temp, setTemp] = useState(null);
   const [timezone, setTimezone] = useState(null);
+  const [daily, setDaily] = useState(null);
 
   const api_call = async (e) => {
     e.preventDefault();
-    const url ="https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&appid=3960092f469b92d98917ccf6db87f3ba&units=metric";
+    const url =
+      "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&appid=3960092f469b92d98917ccf6db87f3ba&units=metric";
     const request = axios.get(url);
     const response = await request;
     const current = await response.data.current;
     console.log(response.data);
-    console.log(current.temp);
+    console.log(current.sunset);
 
     setTimezone(response.data.timezone);
     setTemp(current.temp);
     setFeelsLike(current.feels_like);
-    setSunrise(response.data.sunrise);
-    setSunset(response.data.sunset);
+    setSunrise(current.sunrise);
+    setSunset(current.sunset);
+    setDaily(response.data.daily);
   };
 
   return (
     <div className={classes.container}>
       <weatherContext.Provider
-        value={{ api_call, timezone, temp, feelsLike, sunset, sunrise }}
+        value={{ api_call, timezone, temp, feelsLike, sunset, sunrise, daily }}
       >
         <LeftSide />
         <RightSide />
