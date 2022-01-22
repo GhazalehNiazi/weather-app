b9b8641b49a2fca4a3dff3f27d4ed4bdd80f8e18
GhazalehNiazi <ghazaleniazisabet02@gmail.com>
2022-01-22 14:22:46 +0330

    :deleted useless codes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import "./App.css";
 import React from "react";
 import MainPage from "./components/Pages/MainPage";
-import GlobalState from "./store/GlobalState";
 function App() {
   return (
-    // <GlobalState>
+
       <React.Fragment>
         <MainPage />
       </React.Fragment>
-    // </GlobalState>
+
   );
 }
 
--- src/components/Pages/MainPage.js
+++ src/components/Pages/MainPage.js
@@ -12,11 +12,9 @@ const MainPage = () => {
   const [temp, setTemp] = useState(null);
   const [timezone, setTimezone] = useState(null);
 
-  // useEffect(()=>{
-
   const api_call = async (e) => {
     e.preventDefault();
-    // const url ="https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=3960092f469b92d98917ccf6db87f3ba";
+    const url ="https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=3960092f469b92d98917ccf6db87f3ba";
     const request = axios.get(url);
     const response = await request;
     const current = await response.data.current;
@@ -29,11 +27,8 @@ const MainPage = () => {
     setSunset(response.data.sunset);
   };
 
-  // },[]);
-
   return (
     <div className={classes.container}>
-      <h1>{temp}</h1>
       <weatherContext.Provider
         value={{ api_call, timezone, temp, feelsLike, sunset, sunrise }}
       >
@@ -44,34 +39,3 @@ const MainPage = () => {
   );
 };
 export default MainPage;
-
-// calling the api
-// let componentMounted = true;
-// const fetchWeather = async ()=>{
-//   if(componentMounted){
-//   const response = await fetch(url);
-//     setData(await response.json());
-//     // console.log(response.json());
-//   }
-//   return()=>{
-//     componentMounted = false
-//   }
-// }
-// fetchWeather();
-// if(!componentMounted){
-// console.log(data)}
-
-// context.searchForCity({data:data})
-// console.log(data);
-// console.log(data.timezone) //city name
-// setTimeout(()=>
-// {console.log(data.timezone) //city name
-//   setTimezone(data.timezone);
-//   console.log(timezone);
-//   console.log(data.current.clouds)
-// console.log(data.current.feels_like)
-// console.log(data.current.temp)
-// }
-// ,3000)
-
-// context.searchForCity(data)
