6a3479c336428e436464cc648221a5708fd0d38f
GhazalehNiazi <ghazaleniazisabet02@gmail.com>
2022-01-22 17:19:34 +0330

    :fixed the date

--- src/components/Layouts/RightSide/RightSide.js
+++ src/components/Layouts/RightSide/RightSide.js
@@ -1,21 +1,30 @@
 import { useContext, useState } from "react";
 import weatherContext from "../../../store/weather-context";
+import { weekDays, months } from "../../helper/year";
 import classes from "./RightSide.module.css";
 
 const RightSide = () => {
   const { timezone, temp, feelsLike, sunset, sunrise } =
     useContext(weatherContext);
 
-  const celicusTemp = +temp ;
+  const celicusTemp = +temp;
   const fixedTemp = celicusTemp.toFixed(0);
 
-  const celicusFeelsLike = +feelsLike ;
+  const celicusFeelsLike = +feelsLike;
   const fixedFeelsLike = celicusFeelsLike.toFixed(0);
 
-  const date = new Date();
-  const day = date.getDay();
+  // BUG :: sunset is undefined!! even though it is defined!
+  // const sunsetHour = sunset.getHours();
+  // const sunsetMinute = sunset.getMinutes();
 
-  const month = date.getMonth() + 1;
+  const date = new Date();
+  const dateNumber = date.getDate();
+  const day = date.getDay();
+  const dayName = weekDays[day];
+  const month = date.getMonth();
+  const monthName = months[month];
+
+  // const countryName = timezone.split('/').join(' ');
   return (
     <div className={classes.container}>
       <div>
@@ -23,22 +32,36 @@ const RightSide = () => {
       </div>
       <div className={classes.detail}>
         <div className={classes.topMain}>
+          <img src="https://img.icons8.com/dusk/64/000000/rain--v2.png" />
+          <div className={classes.topMainInfo}>
           <h3>Today</h3>
-          <h5>{day} ,{month}</h5>
+          <h5>
+            {dayName} ,{dateNumber}
+            {monthName}
+          </h5>
+          </div>
         </div>
         <div className={classes.main}>
-          <h1>{fixedTemp}°C</h1>
-          <h5>{timezone}</h5>
+          <h1>
+            {fixedTemp} <span>°C</span>
+          </h1>
+          <h5>{timezone.split('/').join(' ')}</h5>
         </div>
         <div className={classes.underMain}>
           <h5>feels like {fixedFeelsLike}</h5>
-          <h5>sunset {sunrise}</h5>
+          <h5>
+            sunset
+            {/* {sunsetHour}:{sunsetMinute} */}
+          </h5>
         </div>
       </div>
       <div className={classes.rainChance}>
         <h3>chance of rain</h3>
         <div>chart</div>
       </div>
+      {/* <a className={classes.source} href="https://icons8.com/icon/jQd6n8cz0bH5/rain"> */}
+        {/* Rain icon by Icons8
+      </a> */}
     </div>
   );
 };
---
+++ src/components/helper/year.js
@@ -0,0 +1,16 @@
+export const weekDays = ["Mon", "Tue", "Wed", "Thur", "Fri", "	Sat", "Sun"];
+
+export const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May ",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
