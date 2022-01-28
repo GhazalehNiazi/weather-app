e67cde7a432422dd21c1ad2bbf6c0942c5624401
GhazalehNiazi <ghazaleniazisabet02@gmail.com>
2022-01-28 22:40:31 +0330

    :added some photos in image section

--- src/components/Layouts/LeftSide/LeftSide.js
+++ src/components/Layouts/LeftSide/LeftSide.js
@@ -1,7 +1,10 @@
 import SearchBox from "./SearchBox";
 import WeatherReport from "./WeatherReport";
 import classes from "./LeftSide.module.css";
-
+import nyLogo from '../../../assets/ny.jpg';
+import franceLogo from '../../../assets/france.jpg';
+import dubaiLogo from '../../../assets/dubai.jpg';
+import italyLogo from '../../../assets/italy.jpg';
 const LeftSide = () => {
   return (
     <div className={classes.container}>
@@ -10,9 +13,10 @@ const LeftSide = () => {
         Weather <span>Forecast</span>
       </h1>
       <div className={classes.imgContainer}>
-        <div>img</div>
-        <div>img</div>
-        <div>img</div>
+        <div className={classes.image}><img src={nyLogo}></img></div>
+        <div className={classes.image}><img src={franceLogo}></img></div>
+        <div className={classes.image}><img src={dubaiLogo}></img></div>
+        <div className={classes.image}><img src={italyLogo}></img></div>
       </div>
       <WeatherReport />
     </div>
