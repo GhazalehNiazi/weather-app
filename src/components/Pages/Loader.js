fe5c73899ee4d564e158c4620e53e5e94c8d8352
GhazalehNiazi <ghazaleniazisabet02@gmail.com>
2022-01-30 13:15:42 +0330

    :added a lazy loader

---
+++ src/components/Pages/Loader.js
@@ -0,0 +1,5 @@
+import classes from './Loader.module.css'
+const Loader =()=>{
+    return <div className={classes.container}><div className={classes.loader}></div>  </div> 
+}
+export default Loader;
--- src/components/Pages/MainPage.js
+++ src/components/Pages/MainPage.js
@@ -1,13 +1,19 @@
+import { Suspense } from "react/cjs/react.development";
 import { useState } from "react/cjs/react.development";
 import axios from "axios";
-import LeftSide from "../Layouts/LeftSide/LeftSide";
-import RightSide from "../Layouts/RightSide/RightSide";
+// import LeftSide from "../Layouts/LeftSide/LeftSide";
+// import RightSide from "../Layouts/RightSide/RightSide";
 import classes from "./MainPage.module.css";
 import weatherContext from "../../store/weather-context";
 import { weekDays } from "../helper/year";
 import React from "react";
 import WarningModal from "./WarningModal";
-const MainPage = (props) => {
+import Loader from "./Loader";
+
+const LeftSide = React.lazy(() => import("../Layouts/LeftSide/LeftSide"));
+const RightSide = React.lazy(() => import("../Layouts/RightSide/RightSide"));
+
+const MainPage = () => {
   const [sunrise, setSunrise] = useState(null);
   const [sunset, setSunset] = useState(null);
   const [feelsLike, setFeelsLike] = useState(null);
@@ -16,7 +22,7 @@ const MainPage = (props) => {
   const [daily, setDaily] = useState(null);
   const [chartData, setChartData] = useState({});
 
-  const api_call = async (e) => {
+  const api_call = async () => {
     const url =
       "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&appid=3960092f469b92d98917ccf6db87f3ba&units=metric";
     const request = axios.get(url);
@@ -42,31 +48,36 @@ const MainPage = (props) => {
       ],
     });
   };
+
   const [showMain, setShowMain] = useState(false);
+
   const closeHandler = () => {
     setShowMain(true);
     api_call();
   };
+
   return (
     <React.Fragment>
-      <div className={classes.container}>
-        <weatherContext.Provider
-          value={{
-            api_call,
-            timezone,
-            temp,
-            feelsLike,
-            sunset,
-            sunrise,
-            daily,
-            chartData,
-          }}
-        >
-          {!showMain && <WarningModal onClose={closeHandler} />}
-          {showMain && <LeftSide />}
-          {showMain && <RightSide />}
-        </weatherContext.Provider>
-      </div>
+      <Suspense fallback={<Loader/>}>
+        <div className={classes.container}>
+          <weatherContext.Provider
+            value={{
+              api_call,
+              timezone,
+              temp,
+              feelsLike,
+              sunset,
+              sunrise,
+              daily,
+              chartData,
+            }}
+          >
+            {!showMain && <WarningModal onClose={closeHandler} />}
+            {showMain && <LeftSide />}
+            {showMain && <RightSide />}
+          </weatherContext.Provider>
+        </div>
+      </Suspense>
     </React.Fragment>
   );
 };
